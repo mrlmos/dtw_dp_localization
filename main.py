ca21fb2a1fbe1061f06b8d57906e99ede524c70d
Mrlmos <muriloramos710@gmail.com>
2026-04-19 14:52:53 -0300

    add failed experiment

--- main.py
+++ main.py
@@ -11,15 +11,23 @@ def dist(x, y):
 def main():
     path = "/home/murilo/dev/python/tcc/dados_dp/"
     experimentos = load_experiments(base_dir=path, antennas=["antena1"])
-    exp = get_experiment(experimentos, "antena1", 1)
+    exp = get_experiment(experimentos, "antena1", 0)
     # t = exp.time()
     v1 = exp.voltage("CH1")
     v2 = exp.voltage("CH2")
 
     exp.plot()
     plt.show()
+    costs = []
+    values = range(1, 20)
+    values = range(1, len(v1) // 2)
+    for r in values:
+        path, cost = dtw(v1, v2, dist, radius=r)
+        costs.append(cost)
 
-    r = 10
+    r = np.argmin(costs) + 1
+
+    # r = 10
 
     D = gen_cost_matrix(v1, v2, dist, radius=r)
     path, cost = dtw(v1, v2, dist, radius=r)
@@ -31,7 +39,7 @@ def main():
     plt.legend()
 
     plt.subplot(2, 2, 3)
-    plt.plot(v1[path[:, 0]], label="sinal 1")
+    plt.plot(v1, label="sinal 1")
     plt.plot(v2[path[:, 1]], label="sinal 2")
     plt.title("sinais alinhados")
     plt.legend()
